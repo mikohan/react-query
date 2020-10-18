c1996e371a2092254a52ffa8e82c5b9c09ab244b
mikohan <angara99@gmail.com>
2020-10-18 17:21:06 +0300

    Finished markup setting

---
+++ src/components/People.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function People(props) {
+    return (
+        <div>
+            <h1>People component</h1>
+        </div>
+    );
+}
+
+export default People;
---
+++ src/components/Planets.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Planets(props) {
+    return (
+        <div>
+            <h1>Planents component</h1>
+        </div>
+    );
+}
+
+export default Planets;
